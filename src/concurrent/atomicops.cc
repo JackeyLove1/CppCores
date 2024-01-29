c1ff63a7bfe2166535ecc347375179c2ad91de0e
root <root@bytedance.localdomain>
2024-01-30 00:59:12 +0800

    feature: atomic ops

---
+++ src/concurrent/atomicops.cc
@@ -0,0 +1,14 @@
+#include "atomicops.h"
+
+// 32-bit low-level operations on any platform.
+
+inline Atomic32 NoBarrier_CompareAndSwap(volatile Atomic32 *ptr,
+                                         Atomic32 old_value,
+                                         Atomic32 new_value) {
+    Atomic32 prev;
+    __asm__ __volatile__("lock; cmpxchgl %1,%2"
+            : "=a" (prev)
+            : "q" (new_value), "m" (*ptr), "0" (old_value)
+            : "memory");
+    return prev;
+}
